5a1c100cd7b9886131b3a0e993c9b9d0e2c4f504
DemiFiend01 <maggie.mr1606@gmail.com>
2025-12-16 22:24:43 +0100

    Modified comments

--- rover-code/Distance_Alarm/Distance_Alarm.ino
+++ rover-code/Distance_Alarm/Distance_Alarm.ino
@@ -19,38 +19,19 @@ extern "C"{
 } 
 
 void setup() {
-  // put your setup code here, to run once:
-  // pinMode(trigger_uno, OUTPUT);
-  // pinMode(trigger_dos, OUTPUT);
+
   pinMode(echo_uno, INPUT);
   pinMode(echo_dos, INPUT);
 
-  //pinMode(led_uno, OUTPUT);
+  set_output_enable();
+  pinMode(led_uno, OUTPUT); //will need to check the assembly for why it is not setting properly
 
+  Serial.println("Starting to measure the distance: ");
   Serial.begin(115200);
   delay(400);
-  Serial.println("Starting to measure the distance: ");
-  volatile uint32_t* enable_reg = (volatile uint32_t*)0x60004020;
-  Serial.print("PRZED set_output_enable: 0x");
-  Serial.println(*enable_reg, BIN);
-  
-  //pinMode(led_uno, OUTPUT);
-  set_output_enable();
-  pinMode(led_uno, OUTPUT);
-  // Sprawdź PO
-  Serial.print("PO set_output_enable: 0x");
-  Serial.println(*enable_reg, BIN);
-  //set_output_enable();
-
-  
 }
 
 void loop() {
-
-  
-  //digitalWrite(led_uno, LOW);
-  //digitalWrite(trigger_uno,LOW); //just in case of any delays, can later do it in assembly
-  delayMicroseconds(5);
 
   trigger_pin(); //instead of what is below
   // digitalWrite(trigger_uno,HIGH);
@@ -60,44 +41,32 @@ void loop() {
   duration_uno = pulseIn(echo_uno, HIGH);
   distance_uno = 0.0343 * duration_uno / 2; //this will stay in c++ due to 
 
-  //distance = calculate_distance(0.034f, duration);
-
   Serial.print("Distance Uno: "); Serial.println(distance_uno);
   
   if(distance_uno <= alarm_distance)
   {
-    //digitalWrite(led_uno, HIGH);
     led_high();
     delay(100);
   }else
   {
-    //digitalWrite(led_uno, LOW);
     led_low();
     delayMicroseconds(35);
 
   }
 
- trigger_pin_dos();
-  // digitalWrite(trigger_dos, LOW);
-  // delayMicroseconds(5);
-  // digitalWrite(trigger_dos,HIGH);
-  // delayMicroseconds(10);
-  // digitalWrite(trigger_dos,LOW);
+  trigger_pin_dos();
 
-  duration_dos = pulseIn(echo_dos, HIGH); //to do in .S - read analog value from pin number 0
+  duration_dos = pulseIn(echo_dos, HIGH);
   distance_dos = 0.0343 * duration_dos / 2; //this will stay in c++ due to 
   Serial.print("Distance Dos: "); Serial.println(distance_dos);
   if(distance_dos <= alarm_distance)
   {
-    //digitalWrite(led_uno, HIGH);
     led_high();
     delay(200);
   }else
   {
-    //digitalWrite(led_uno, LOW);
     led_low();
     delayMicroseconds(35);
   }
-  
-  //delay(200);
+
 }
