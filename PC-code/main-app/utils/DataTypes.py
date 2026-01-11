5fa1aec8366c5a49350a80cbde53e5af0f95db40
DemiFiend01 <maggie.mr1606@gmail.com>
2026-01-11 15:49:45 +0100

    new comments for some utils

--- PC-code/main-app/utils/DataTypes.py
+++ PC-code/main-app/utils/DataTypes.py
@@ -1,18 +1,29 @@
+"""
+This module provides x
+It contains two classes for data storage.
+"""
 from typing import List
 
 class LidarScan:
-    '''
-    Stores data received from a single LiDAR scan.
-
-    '''
+    """Stores data received from a single LiDAR scan."""
     def __init__(self, bearing : float, position : List[float], payload : List[float]):
-        self.bearing = bearing   # Bearing of the scan
-        self.position = position # Position when the scan was being made
-        self.payload = payload   # Main payload of the scan in a serialized form (r, phi, theta, ...). Flat array.
+        """LidarScan initialisation.
+
+        Keyword arguments:
+            bearing -- Bearing of the scan.
+            position -- Position when the scan was being made.
+            payload -- Main payload of the scan in a serialized form (r, phi, theta, ...). Flat array.
+        """
+        self.bearing = bearing
+        self.position = position
+        self.payload = payload
 
 class OdometryData:
-    '''
-    Stores data received from odometry measurements.
-    '''
+    """Stores data received from odometry measurements."""
     def __init__(self, payload : List[float]):
+        """OdometryData initialisation.
+
+        Keyword arguments:
+            payload --
+        """
         self.payload = payload
--- PC-code/main-app/utils/OperatingSystemCheck.py
+++ PC-code/main-app/utils/OperatingSystemCheck.py
@@ -1,3 +1,6 @@
+"""
+This module contains three constant values regarding the user's operating system.
+"""
 import platform
 
 OS_SYSTEM, OS_RELEASE, OS_VERSION = platform.system_alias(system=platform.system(), release=platform.release(), version=platform.version())
